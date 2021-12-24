abba085457d3b93e917862ea65fb29c229efb8e7
GurkanCeylan <96624294+GurkanCeylan@users.noreply.github.com>
2021-12-24 17:36:31 +0300

    CountriesApp

---
+++ KotlinCountries/app/src/main/java/com/example/kotlincountries/adapter/CountryAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.kotlincountries.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincountries.view.FeedFragmentDirections
+import com.example.kotlincountries.R
+import com.example.kotlincountries.databinding.ItemCountryBinding
+import com.example.kotlincountries.model.Country
+import com.example.kotlincountries.util.downloadFromUrl
+import com.example.kotlincountries.util.placeholderProgressBar
+import kotlinx.android.synthetic.main.item_country.view.*
+
+class CountryAdapter(val countryList : ArrayList<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>(), CountryClickListener {
+
+    class CountryViewHolder(var view : ItemCountryBinding) : RecyclerView.ViewHolder(view.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        //val view = inflater.inflate(R.layout.item_country, parent, false)
+        val view = DataBindingUtil.inflate<ItemCountryBinding>(inflater, R.layout.item_country, parent, false)
+        return CountryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+
+        holder.view.country = countryList[position]
+        holder.view.listener = this
+
+        /*
+        holder.view.itemName.text = countryList[position].countryName
+        holder.view.itemRegion.text = countryList[position].countryRegion
+        holder.view.itemImage.downloadFromUrl(countryList[position].imageUrl, placeholderProgressBar(holder.view.context))
+
+        holder.view.setOnClickListener {
+            val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment(countryList[position].uuid)
+            Navigation.findNavController(it).navigate(action)
+        }
+        */
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+    fun updateCountryList(newCountryList: List<Country>){
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCountryClicked(v: View) {
+        val uuid = v.countryUuidText.text.toString().toInt()
+        val action = FeedFragmentDirections.actionFeedFragmentToCountryFragment(uuid)
+        Navigation.findNavController(v).navigate(action)
+    }
+
+}
---
+++ KotlinCountries/app/src/main/java/com/example/kotlincountries/viewmodel/CountryViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.kotlincountries.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kotlincountries.model.Country
+import com.example.kotlincountries.service.CountryDatabase
+import kotlinx.coroutines.launch
+import java.util.*
+
+class CountryViewModel(application: Application) : BaseViewModel(application) {
+
+    val countryLiveData = MutableLiveData<Country>()
+
+    fun getDataFromRoom(uuid: Int){
+        launch {
+
+            val dao = CountryDatabase(getApplication()).countryDao()
+            val country = dao.getCountry(uuid)
+            countryLiveData.value = country
+        }
+    }
+}
